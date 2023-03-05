4bc3896273f5868ccd420674aa1faaf9cafc49db
valerijadrinek <valerijadrinek@gmail.com>
2023-03-05 13:28:22 +0100

    new version

--- Nova mapa/script.js
+++ Nova mapa/script.js
@@ -1,12 +1,12 @@
 
 // Import Swiper and modules
-import Swiper, { Navigation, } from 'assets/swiper';
+import Swiper from 'swiper';
 
 // Now you can use Swiper
 const swiper = new Swiper('.swiper', {
     slidesPerView: 3,
     speed: 500,
-    spaceBetween: 500,
+    spaceBetween: 100,
   // Install modules
   modules: [Navigation],
   
